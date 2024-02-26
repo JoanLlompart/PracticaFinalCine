5a993f9a182a7cfa96f809ee363df7c95ccbcae7
Andres Carrasco Dols <acarrascod@esliceu.net>
2024-02-26 09:23:17 +0100

    cambios

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,3 @@
-
 // JavaScript para consumir la API y mostrar las imágenes de las películas
 const apiKey = '94b49414abe2bf22eaea7d2de2623815'; // Reemplaza 'TU_API_KEY' con tu clave de API
 
@@ -15,7 +14,7 @@ async function fetchMovies() {
         movieContainer.innerHTML = '';
 
         // Iterar sobre las películas y crear tarjetas para cada una
-        movies.forEach(movie => {
+        movies.forEach(async movie => {
             const movieCard = document.createElement('div');
             movieCard.classList.add('movie-card');
 
@@ -23,11 +22,41 @@ async function fetchMovies() {
             image.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
             image.alt = movie.title;
 
+            const title = document.createElement('h2');
+            title.textContent = movie.title;
+
+            const description = document.createElement('p');
+            description.textContent = movie.overview;
+
+            const genre = document.createElement('p');
+            genre.textContent = 'Género: ' + await getGenreNames(movie.genre_ids);
+
             movieCard.appendChild(image);
+            movieCard.appendChild(title);
+            movieCard.appendChild(genre);
+            //movieCard.appendChild(description);
+
             movieContainer.appendChild(movieCard);
         });
     } catch (error) {
         console.error('Error al obtener las películas:', error);
+    }
+}
+
+// Función para obtener los nombres de género a partir de los identificadores de género
+async function getGenreNames(genreIds) {
+    try {
+        const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=es`);
+        const data = await response.json();
+        const genres = data.genres;
+
+        return genreIds.map(genreId => {
+            const genre = genres.find(genre => genre.id === genreId);
+            return genre ? genre.name : 'Desconocido';
+        }).join(', ');
+    } catch (error) {
+        console.error('Error al obtener los nombres de género:', error);
+        return 'Desconocido';
     }
 }
 
